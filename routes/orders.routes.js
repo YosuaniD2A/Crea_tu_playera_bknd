fbf8eefdfb29a5b2bc8783bb8c15a8540b11a083
YosuaniD2A <yosuani@d2america.com>
2024-04-02 13:45:32 -0700

    cambiando a POST

--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -2,7 +2,7 @@ var express = require('express');
 const { updateStatus } = require('../controllers/orders.controller');
 var router = express.Router();
 
-router.put('/updateStatus/:orderID', updateStatus);
+router.post('/updateStatus/:orderID', updateStatus);
 
 
 module.exports = router;
