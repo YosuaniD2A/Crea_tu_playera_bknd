751fc89cfc2b3c37fcbb904ee3d2309476811f7e
YosuaniD2A <yosuani@d2america.com>
2024-04-05 11:02:18 -0700

    Actualizado a un PUT

--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -2,7 +2,7 @@ var express = require('express');
 const { updateStatus, getKornitXResponses } = require('../controllers/orders.controller');
 var router = express.Router();
 
-router.post('/updateStatus/:orderID', updateStatus);
+router.put('/updateStatus/:orderID', updateStatus);
 
 router.get('/getKornitXResponses/', getKornitXResponses);
 
